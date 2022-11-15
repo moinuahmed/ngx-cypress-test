08a78fe197a2b51830c74f08af31f2202cfe8ce6
moinuahmed <moinuahmed2011@gmail.com>
2022-11-15 12:35:03 +0600

    fix:saving subject of the command

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -73,7 +73,30 @@ describe('Our first suite', () => {
         cy.contains('Forms').click()
         cy.contains('Form Layouts').click()
 
-        cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain','Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain','Email')
+        // cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain','Password')
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain','Email address') 
+        // cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain','Password')
+
+
+        cy.contains('nb-card', 'Using the Grid').then( firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+
+            cy.contains('nb-card', 'Basic form').then( secondForm => {
+                const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).to.equal(passwordSecondText)
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain','Password')
+
+
+
+            })
+
+        })
 
     })
 })
